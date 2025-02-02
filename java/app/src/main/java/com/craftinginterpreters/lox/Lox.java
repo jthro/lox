0dd370677c82dccc66731371e15a06930d3deee5
jthro <jethro.rosette@outlook.com>
2025-02-02 15:59:06 +1100

    End chapter 6 parsing expressions

--- java/app/src/main/java/com/craftinginterpreters/lox/Lox.java
+++ java/app/src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -11,7 +11,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class Lox {
+class Lox {
 
     static boolean hadError = false;
 
@@ -49,10 +49,12 @@ public class Lox {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
 
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        if (hadError) return;
+
+        System.out.println(new AstPrinter().print(expression));
     }
 
     static void error(int line, String message) {
@@ -62,5 +64,13 @@ public class Lox {
     private static void report(int line, String where, String message) {
         System.err.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 }
