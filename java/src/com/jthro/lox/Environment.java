af4a5496bf68fa62b84312c46cd8b2f7827fa61a
jthro <jethro.rosette@outlook.com>
2025-03-21 17:17:25 +1100

    Chapter 11 Resolving compmlete

--- java/src/com/jthro/lox/Environment.java
+++ java/src/com/jthro/lox/Environment.java
@@ -7,15 +7,15 @@ public class Environment {
     final Environment enclosing;
     private final Map<String, Object> values = new HashMap<>();
 
-    Environment() {
+    public Environment() {
         enclosing = null;
     }
 
-    Environment(Environment enclosing) {
+    public Environment(Environment enclosing) {
         this.enclosing = enclosing;
     }
 
-    Object get(Token name) {
+    public Object get(Token name) {
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
         }
@@ -42,5 +42,21 @@ public class Environment {
 
     void define(String name, Object value) {
         values.put(name, value);
+    }
+
+    public Environment ancestor(int distance) {
+        Environment environment = this;
+        for (int i = 0; i < distance; i++) {
+            environment = environment.enclosing;
+        }
+        return environment;
+    }
+
+    Object getAt(int distance, String name) {
+        return ancestor(distance).values.get(name);
+    }
+
+    void assignAt(int distance, Token name, Object value) {
+        ancestor(distance).values.put(name.lexeme, value);
     }
 }
